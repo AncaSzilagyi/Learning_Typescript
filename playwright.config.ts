1f64fcc3e56ddaf5771f32c5bf91ab39707b3479
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-01-23 11:44:16 +0200

    New tests with codegen

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,10 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  testMatch: ["tests/Login.test.ts"]
+  testMatch: ["tests/recorded.test.ts"],  
+  use: {
+    headless: false
+  }
 };
 
 export default config;
