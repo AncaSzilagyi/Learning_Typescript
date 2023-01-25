979e93e50bbf70f95a13c1a5bd0af071456f3944
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-01-25 12:14:34 +0200

    Added alerts processing

--- tests/alertsHandling.test.ts
+++ tests/alertsHandling.test.ts
@@ -1,6 +1,15 @@
-import {test } from "@playwright/test";
+import {expect, test } from "@playwright/test";
 
 test("handling alerts", async ({page}) => {
     await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo');
-    await page.locator("button:has-text('Click Me')").nth(0).click();
+    
+
+    page.on("dialog", async (alert) => {
+        const text = await alert.message();
+        console.log(text);
+        await alert.dismiss();
+    })
+
+    await page.locator("button:has-text('Click Me')").nth(1).click();
+    expect(page.locator("id=confirm-demo")).toContainText("Cancel!");
 })
