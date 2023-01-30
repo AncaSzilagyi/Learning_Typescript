3b9006906c533c6dd04a95b521b6684bea8ddba4
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-01-30 11:35:12 +0200

    Updated frames tests

--- tests/frames.test.ts
+++ tests/frames.test.ts
@@ -4,4 +4,13 @@ test("Interact with frames", async ({ page }) => {
     await page.goto("https://letcode.in/frame");
     const allFrames = page.frames();
     console.log("No of frames: " + allFrames.length);
+
+    const firstFrame = page.frame("firstFr");
+    await firstFrame?.fill("input[name='fname']", "Elisa");
+    //question mark considers the case
+    //where there is no frame
+
+    await firstFrame?.fill("input[name='lname']", "test");
+
+    await page.waitForTimeout(3000);
 })
