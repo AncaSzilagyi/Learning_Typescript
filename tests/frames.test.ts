6219530ee82afa2e8e46a0e5f3bc2f3dd2b9687d
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-01-30 11:57:22 +0200

    Updated frames tests

--- tests/frames.test.ts
+++ tests/frames.test.ts
@@ -7,20 +7,23 @@ test("Interact with frames", async ({ page }) => {
 
     //First way to assert:
 
-    const myFrame = page.frame("firstFr");
-    var firstName = "Elisa";
-    await myFrame?.fill("input[name='fname']", firstName);
-    //question mark considers the case
-    //where there is no frame
-    var lastName = "test";
-    await myFrame?.fill("input[name='lname']", lastName);
-    expect(await myFrame?.locator("//p[@class='title has-text-info']").textContent()).toContain('You have entered ' + firstName + " " + lastName);
+    // const myFrame = page.frame("firstFr");
+    // var firstName = "Elisa";
+    // await myFrame?.fill("input[name='fname']", firstName);
+    // //question mark considers the case
+    // //where there is no frame
+    // var lastName = "test";
+    // await myFrame?.fill("input[name='lname']", lastName);
+    // expect(await myFrame?.locator("//p[@class='title has-text-info']").textContent()).toContain('You have entered ' + firstName + " " + lastName);
 
     //Second way to assert:
 
-    // const myFrame = page.frameLocator("#firstFr");
-    // await myFrame.locator("input[name='fname']").fill("Elisa"); 
-    // await myFrame.locator("input[name='lname']").fill("test");
+    const myFrame = page.frameLocator("#firstFr");
+    await myFrame.locator("input[name='fname']").fill("Elisa"); 
+    await myFrame.locator("input[name='lname']").fill("test");
+
+    const innerFrame = myFrame.frameLocator("iframe[src='innerFrame']");
+    await innerFrame.locator("input[name='email']").fill("elisa@test.com");
 
 
     await page.waitForTimeout(3000);
