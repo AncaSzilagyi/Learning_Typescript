cf6369d07aeaa21c7675e7dd97aeb5a63e0f8a0f
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-01-30 15:42:01 +0200

    Added handling multiple tabs commands

--- tests/windows.test.ts
+++ tests/windows.test.ts
@@ -2,13 +2,20 @@ import { expect, test } from "@playwright/test";
 
 test("Interact with multiple tabs", async ({ page }) => {
     await page.goto("https://www.lambdatest.com/selenium-playground/window-popup-modal-demo");
-    
+
     console.log(page.url());
-    
-    const [newWindow] = await Promise.all([
+
+    const [multiplePages] = await Promise.all([
         page.waitForEvent("popup"),
-        page.click("'Follow On Twitter'")
+        page.click("#followboth")
     ]);
 
-    console.log(newWindow.url());
+    const pages = multiplePages.context().pages(); //gives all the pages.
+    console.log("No. of tabs is: " + pages.length);
+    // const [newWindow] = await Promise.all([
+    //     page.waitForEvent("popup"),
+    //     page.click("'Follow On Twitter'")
+    // ]);
+
+    // console.log(newWindow.url());
 })
