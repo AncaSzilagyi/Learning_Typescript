a036fb86f2530d2edd35cf051fe45a87acbea9ba
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-02-02 11:16:37 +0200

    Added trace option

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,14 +2,15 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  testMatch: ["tests/meetupExamples.test.ts"],
+  testMatch: ["tests/meetupEx2.test.ts"],
   use: {
     headless: false,
     screenshot: "on",
     video: "retain-on-failure",
     launchOptions: {
       slowMo: 1000
-    }
+    },
+    trace: 'retain-on-failure' //debugging option
   },
   retries: 0,
   reporter: [["dot"], ["json", {
