c36f207836ef3edc4186703129be522fcd09e234
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-02-02 13:41:31 +0200

    updated download test

--- tests/uploadDownload.test.ts
+++ tests/uploadDownload.test.ts
@@ -6,4 +6,10 @@ test("Download files", async ({ page }) => {
     await page.click("//button[@id='create']");
     await page.screenshot();
     await page.click("#link-to-download");
+    const download = await Promise.all([
+        page.waitForEvent("download"),
+        page.click("id=link-to-download")
+    ]);
+    const path = await download[0].path();
+    console.log(path);
 })
