97a596d5e56575cd72a2040a23c506590b2d9c0d
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-02-02 14:32:50 +0200

    Updated download functions

--- tests/uploadDownload.test.ts
+++ tests/uploadDownload.test.ts
@@ -10,6 +10,18 @@ test("Download files", async ({ page }) => {
         page.waitForEvent("download"),
         page.click("id=link-to-download")
     ]);
-    const path = await download[0].path();
-    console.log(path);
+    const fileName = download[0].suggestedFilename();
+    await download[0].saveAs(fileName);
+    // const path = await download[0].path();
+    // console.log(path);
+    //Downloaded files are deleted when the browser context that produced them is closed.
+
+})
+
+test.only("Upload file", async ({ page }) => {
+    await page.goto("https://blueimp.github.io/jQuery-File-Upload/");
+    await page.setInputFiles("//input[@type='file']",
+        ["itemsToUpload/dog.png", "itemsToUpload/bear.png"]);
+        // these files are too large, but this is how you upload files
+
 })
