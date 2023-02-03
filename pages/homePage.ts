e1ab3f15e4148a425d5f8a45ef1c3c0951a5a545
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-02-03 12:57:04 +0200

    added homepage

---
+++ pages/homePage.ts
@@ -0,0 +1,14 @@
+import { Page } from "@playwright/test"
+export default class HomePage {
+    constructor(public page: Page) {
+    }
+    async clickOnSpecialHotMenu() {
+        await this.page.click("'Special Hot'");
+        
+    }
+
+
+
+
+
+}
