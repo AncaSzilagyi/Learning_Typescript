92fdcb596132b94c33a6e9ea3a23cea994399195
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-02-03 12:57:20 +0200

    Updated Registration Page functions

--- pages/registrationPage.ts
+++ pages/registrationPage.ts
@@ -4,37 +4,46 @@ export default class RegisterPage {
 
     }
     async enterFirstName(firstname: string) {
-        this.page.locator("#input-firstname")
-            .type(firstname);
+        // this.page.locator("//input[@name='firstname']")
+        //     .fill(firstname);
+        await this.page.type("//input[@name='firstname']", firstname, {delay:100})
     }
     async enterLastName(lastname: string) {
-        this.page.locator("#input-lastname")
-            .type(lastname);
+        // this.page.locator("//input[@name='lastname']")
+        //     .fill(lastname);
+        await this.page.type("//input[@name='lastname']", lastname, {delay:100})
     }
     async enterEmail(email: string) {
-        this.page.locator("#input-email")
-            .type(email);
+        // this.page.locator("//input[@name='email']")
+        //     .fill(email);
+        await this.page.type("//input[@name='email']", email, {delay:100})
     }
     async enterTelephone(phone: string) {
-        this.page.locator("#input-telephone")
-            .type(phone);
+        // this.page.locator("//input[@name='telephone']")
+        //     .fill(phone);
+        await this.page.type("//input[@name='telephone']", phone, {delay:100})
     }
     async enterPassword(password: string) {
-        this.page.locator("#input-password")
-            .type(password);
+        // this.page.locator("//input[@name='password']")
+        //     .fill(password);
+        await this.page.type("//input[@name='password']", password, {delay:100})
     }
     async enterConfirmPassword(password: string) {
-        this.page.locator("#input-confirm")
-            .type(password);
+        // this.page.locator("//input[@name='confirm']")
+        //     .fill(password);
+        await this.page.type("//input[@name='confirm']", password, {delay:100})
     }
-    async isSubscribed() {
-        return await this.page.locator("#input-newsletter-no").isChecked();
+    isSubscribedChecked() {
+        return this.page.locator("#input-newsletter-no");
     }
     async clickTermsAndConditions() {
-        await this.page.click("#input-agree");
+        await this.page.click("//input[@name='agree']");
     }
     async clickContinueToRegister() {
-        await this.page.click("//input[@value='Continue']");
-    }
+        await Promise.all([
+            this.page.waitForNavigation({ waitUntil: "networkidle" }),
+            await this.page.click("//input[@value='Continue']")
+        ]);
 
+    }
 }
