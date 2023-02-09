e32c285a1dd8ca6a277d65ed8824c0b1954fa7ab
elisa.szilagyi <elisa.szilagyi@altom.com>
2023-02-09 15:28:34 +0200

    Added change password test

--- pomtests/accountTests.test.ts
+++ pomtests/accountTests.test.ts
@@ -19,20 +19,49 @@ test.describe("Modify personal information", async () => {
         await homePage.clickEditAccountInfo();
         await myAccount.modifyFirstName(updatedFirstName);
         await myAccount.clickContinueToRegister();
-        await expect (page.locator("//div[@class='alert alert-success alert-dismissible']")).toHaveText(" Success: Your account has been successfully updated.");
+        await expect(page.locator("//div[@class='alert alert-success alert-dismissible']")).toHaveText(" Success: Your account has been successfully updated.");
         await homePage.clickEditAccountInfo();
 
         expect(await myAccount.getFirstName()).toEqual(updatedFirstName);
 
         await myAccount.modifyLastName(updatedLastName);
         await myAccount.clickContinueToRegister();
-        await expect (page.locator("//div[@class='alert alert-success alert-dismissible']")).toHaveText(" Success: Your account has been successfully updated.");
+        await expect(page.locator("//div[@class='alert alert-success alert-dismissible']")).toHaveText(" Success: Your account has been successfully updated.");
         await homePage.clickEditAccountInfo();
 
         expect(await myAccount.getFirstName()).toEqual(updatedFirstName);
         expect(await myAccount.getLastName()).toEqual(updatedLastName);
 
     })
+
+    test("Change password | test_02", async ({ page, baseURL }) => {
+        const login = new LoginPage(page);
+        const homePage = new HomePage(page);
+        const myAccount = new MyAccount(page);
+        const newPassword = "updated password";
+        await page.goto(`${baseURL}route=account/login`);
+        await login.login(email, password);
+
+        await homePage.clickChangePassword();
+        await myAccount.inputNewPassword(newPassword);
+        await myAccount.inputNewPasswordConfirm(newPassword);
+        await myAccount.clickContinueToRegister();
+        await expect(page.locator("//h2[@class='card-header h5']")
+            .nth(0))
+            .toHaveText("My Account");
+        // await homePage.hoverMyAccount();
+        await homePage.logout();
+        await page.click("//a[contains(text(),'Continue')]");
+
+        await page.goto(`${baseURL}route=account/login`);
+        await login.login(email, newPassword);
+        await expect(page.locator("//h2[@class='card-header h5']").nth(0)).toHaveText("My Account");
+
+        await homePage.clickChangePassword();
+
+    })
+
+
 
 
     // test("Edit account info | test_02", async ({ page, baseURL }) => {
